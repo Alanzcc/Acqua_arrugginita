b1c960fa225e28dee9e982d9b8e2b858096783d7
alanzcc <alanzcc1@gmail.com>
2024-08-12 21:03:30 -0300

    Feat: Transformations complete aside from refletion

--- src/painting/shapes.rs
+++ src/painting/shapes.rs
@@ -15,4 +15,74 @@ impl Polygon {
 
     pub fn len(&self) -> usize { self.vertices.len() }
 
+    pub fn stretch_x(&self, k: i32) -> Polygon {
+        let mut new_pol = Polygon::new(vec![]);
+        for v in &self.vertices {
+            new_pol.add_vertex(Point::new(v.x * k, v.y));
+        }
+        new_pol
+    }
+
+    pub fn stretch_y(&self, k: i32) -> Polygon {
+        let mut new_pol = Polygon::new(vec![]);
+        for v in &self.vertices {
+            new_pol.add_vertex(Point::new(v.x, v.y * k));
+        }
+        new_pol
+    }
+
+    pub fn squeeze(&self, k: i32) -> Polygon {
+        let mut new_pol = Polygon::new(vec![]);
+        for v in &self.vertices {
+            new_pol.add_vertex(Point::new(v.x * k, v.y / k));
+        }
+        new_pol
+    }
+
+    pub fn rotate(&self, t: f32) -> Polygon {
+        let mut new_pol = Polygon::new(vec![]);
+        for v in &self.vertices {
+            new_pol.add_vertex(Point::new(
+                ((v.x as f32) * t.cos() - (v.y as f32) * t.sin()) as i32,
+                ((v.x as f32) * t.sin() + (v.y as f32) * t.cos()) as i32));
+        }
+        new_pol
+    }
+    pub fn shear_x(&self, k: i32) -> Polygon {
+        let mut new_pol = Polygon::new(vec![]);
+        for v in &self.vertices {
+            new_pol.add_vertex(Point::new(v.x + v.y * k, v.y));
+        }
+        new_pol
+    }
+
+    pub fn shear_y(&self, k: i32) -> Polygon {
+        let mut new_pol = Polygon::new(vec![]);
+        for v in &self.vertices {
+            new_pol.add_vertex(Point::new(v.x, v.y + v.x * k));
+        }
+        new_pol
+    }
+
+    pub fn scale(&self, k: i32) -> Polygon {
+        let mut new_pol = Polygon::new(vec![]);
+        for v in &self.vertices {
+            new_pol.add_vertex(Point::new(v.x * k, v.y * k));
+        }
+        new_pol
+    }
+
+    fn length(v: Point) -> i32 { (v.x * v.x + v.y + v.y) as f32.sqrt() as i32 }
+
+    fn scale_reflect(lv: Vec<i32>, k: i32) -> Vec<i32> {}
+    fn reflect(l: Point) {
+        let ll = Self::length(l);
+        let lv = vec![
+            l.x * l.x - l.y * l.y, 2 * l.x * l.y
+            2 * l.x * l.y, l.y * l.y + l.x * l.x];
+    }
+    pub fn reflection(&self) -> Polygon {
+        let mut new_pol = Polygon::new(vec![]);
+        for v in &self.vertices {}
+    }
 }
